2fd32528cf7b40db1ae66227d113acc6a21b81b9
arbdt <67365149+arbdt@users.noreply.github.com>
2020-11-08 15:16:41 +1030

    added sql for adding to tables

--- EmployeeDB.js
+++ EmployeeDB.js
@@ -8,40 +8,41 @@ class EmployeeDB {
             user: "root",
             password: "rocket"
         });
-        this.connection.connect(function(err) {
-            if (err) throw err;
-            console.log("Connection established.");
+        //this.connection.connect(function(err) {
+            //if (err) throw err;
+            //console.log("Connection established.");
             this.connection.query("CREATE DATABASE IF NOT EXISTS employeeDB", function (err, result) {
                 if (err) throw err;
-                console.log("Database created");
+                //console.log("Database created");
             });
-        });
-        this.connection.query("USE employeeDB", function(err, res){
-            if (err) throw err;
-            console.log("Database connected");
-        });
+            this.connection.query("USE employeeDB", function(err, res){
+                if (err) throw err;
+                //console.log("Database connected");
+            });
+        //});
+
     }
 
     // create department table
     createDepartmentTable(){
-        this.connection.query("CREATE TABLE IF NOT EXISTS department (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30))"),
+        this.connection.query("CREATE TABLE IF NOT EXISTS department (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30))",
         function(error, result){
             if (error) throw error;
-            console.log("Department table created.");
-        };
+            //console.log("Department table created.");
+        });
     }
 
     // create role table
     createRoleTable(){
         this.connection.query(`CREATE TABLE IF NOT EXISTS role (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
             title VARCHAR(30),
-            salary DECIMAL,
+            salary DECIMAL(10,2),
             department_id INT NOT NULL,
-            FOREIGN KEY(department_id) REFERENCES department(id))`),
+            FOREIGN KEY(department_id) REFERENCES department(id))`,
         function(error, result){
             if (error) throw error;
-            console.log("Role table created.");
-        };
+            //console.log("Role table created.");
+        });
     }
 
     // create employee table
@@ -51,11 +52,52 @@ class EmployeeDB {
             last_name VARCHAR(30),
             role_id INT NOT NULL,
             manager_id INT,
-            FOREIGN KEY(role_id) REFERENCES role(id))`),
+            FOREIGN KEY(role_id) REFERENCES role(id))`,
         function(error, result){
             if (error) throw error;
-            console.log("Employee table created.")
-        }
+            //console.log("Employee table created.")
+        });
+    }
+
+    // function for adding department
+    addDepartment(deptName){
+        this.connection.query(`INSERT INTO department SET ?`, {
+            name: deptName
+        }, function(error, result){
+            if (error) throw error;
+            console.log(`${result.affectedRows} department added.`);
+        });
+    }
+
+    // function for adding role
+    addRole(roleTitle, roleSalary, roleDepartment){
+        let departmentID;
+        // get department reference
+        this.connection.query(`SELECT id FROM department WHERE ?`, {
+            name: roleDepartment
+        }, function (error, result){
+            if (error) throw error;
+            console.log(result);
+        });
+
+        this.connection.query(`INSERT INTO role SET ?`, {
+            title: roleTitle,
+            salary: roleSalary,
+            department_id: departmentID
+        }, function(error, result){
+            if (error) throw error;
+            console.log(`${result.affectedRows} department added.`);
+        });
+    }
+
+    // function for adding employee
+    addEmployee(firstNameValue, lastNameValue, roleTitle, managerFullName){
+        this.connection.query(`INSERT INTO employee SET ?`, {
+            name: deptName
+        }, function(error, result){
+            if (error) throw error;
+            console.log(`${result.affectedRows} department added.`);
+        });
     }
 
 }
